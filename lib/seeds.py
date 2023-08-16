7dcae7e302d2a681c23ea112bd89411604e9a1ac
Jory Roberts <robertsjory13@gmail.com>
2023-08-15 23:32:38 -0500

    progress on seeds create_venues

--- lib/seeds.py
+++ lib/seeds.py
@@ -11,8 +11,47 @@ from faker import Faker
 
 fake = Faker()
 
+print("🎸 Seeding DB..")
 
-def delete_records(self):
+venue_names = [
+    "Red Rocks Park and Amphitheatre",
+    "First Avenue",
+    "The Thirsty Duck",
+    "Ryman Auditorium",
+    "The Bluebird Cafe",
+    "Brew Works",
+    "The Basement",
+    "The Emerald Theater",
+    "Pabst Theater",
+    "Turner Hall Ballroom",
+]
+
+
+def delete_records():
     session.query(Artist).delete()
     session.query(Venue).delete()
     session.commit()
+
+
+def create_venues():
+    venues = []
+    for i in range(10):
+        venue = Venue(
+            venue_name = venue_names,
+            venue_email = fake.email(),
+            venue_address = fake.street_address(),
+            venue_city = fake.city(),
+            venue_state = fake.state(),
+            venue_zip_code = fake.postcode(),
+            capacity = fake.random_int(min=50, max=1000)
+        )
+
+        session.add(venue)
+        session.commit()
+        venues.append(venue)
+    return venues
+
+
+
+
+print("Success! 🤘")
