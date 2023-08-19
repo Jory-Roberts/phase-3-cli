e5df8e68491fd1cbb23e3b287f11602ecea6aac0
Jory Roberts <robertsjory13@gmail.com>
2023-08-19 13:57:55 -0500

    cleans up comments adds create and delete venue methods

--- lib/models/helper.py
+++ lib/models/helper.py
@@ -1,6 +1,8 @@
 from models import Session, Artist, Venue, Booking
 from datetime import datetime
 
+session = Session()
+
 
 ##query by artist name
 ## if artist name available, create new artist entry
@@ -15,8 +17,6 @@ def create_artist_entry(
     genre,
     availability_str,
 ):
-    session = Session()
-
     existing_artist = session.query(Artist).filter_by(artist_name=artist_name).first()
 
     if existing_artist is None:
@@ -40,12 +40,10 @@ def create_artist_entry(
         print(f"Artist: {artist_name} already exists")
 
 
-##filter by artist name
-##allow artist to update email, phone_number
-## return confirmed
+# filter by artist name
+# allow artist to update email, phone_number
+# return confirmed
 def update_artist_contact(artist_name, new_email, new_phone_number):
-    session = Session()
-
     update_existing_artist = (
         session.query(Artist).filter_by(artist_name=artist_name).first()
     )
@@ -53,19 +51,21 @@ def update_artist_contact(artist_name, new_email, new_phone_number):
     if update_existing_artist:
         update_existing_artist.email = new_email
         update_existing_artist.phone_number = new_phone_number
+
         print(
             f"Artist: {artist_name} contact information has been updated successfully!"
         )
+
         session.add(update_existing_artist)
         session.commit()
+
     else:
         print(f"Artist: {artist_name} not found!")
 
 
-##query by artist name
-## if name matches remove
+# query by artist name
+# if name matches remove
 def remove_artist(artist_name):
-    session = Session()
     remove_current_artist = (
         session.query(Artist).filter_by(artist_name=artist_name).first()
     )
@@ -73,6 +73,7 @@ def remove_artist(artist_name):
     if remove_current_artist:
         session.delete(remove_current_artist)
         session.commit()
+
         print(f"Artist: {artist_name} removed!")
     else:
         print(f"Artist: {artist_name} does not exist. Try again!")
@@ -81,8 +82,6 @@ def remove_artist(artist_name):
 ##filter by availability date and artist name
 ## if date matches return message available
 def artist_availability(artist_name, availability):
-    session = Session()
-
     check_artist_availability = (
         session.query(Artist)
         .filter_by(artist_name=artist_name, availability=availability)
@@ -99,64 +98,89 @@ def artist_availability(artist_name, availability):
 ## query venue by name and capacity
 ## allow check of venue capacity
 def check_venue_capacity(venue_name, capacity):
-    session = Session()
-
     check_venue_capacity = (
         session.query(Venue).filter_by(venue_name=venue_name, capacity=capacity).first()
     )
 
     if check_venue_capacity:
         session.commit()
+
         print(f"Venue: {venue_name} has {capacity} seats available for seating")
     else:
-        print(f"Venue: Unable to find {venue_name} with {capacity} provided")
+        print(f"Venue: Unable to find {venue_name} with {capacity} seating provided")
 
 
 ##query venue by name and capacity
 ##update venue capacity
 def update_venue_capacity(venue_name, capacity, new_capacity):
-    session = Session()
-
     current_venue_capacity = (
         session.query(Venue).filter_by(venue_name=venue_name, capacity=capacity).first()
     )
+
     if current_venue_capacity:
         current_venue_capacity.capacity = new_capacity
         print(
-            f"Venue: {venue_name} has updated to {new_capacity} for seating capacity."
+            f"Venue: {venue_name} has been updated to {new_capacity} for seating capacity."
         )
         session.commit()
     else:
         print(f"Venue: {venue_name} capacity has not been updated.")
 
 
-def create_new_venue():
-    ##filter by venue name
-    ## if venue name is available create new venue
-    pass
+# filter by venue name
+# if venue name is available create new venue
+def create_new_venue(
+    venue_name, venue_email, venue_city, venue_state, venue_zip_code, capacity
+):
+    existing_venue = session.query(Venue).filter_by(venue_name=venue_name).first()
+
+    if existing_venue is None:
+        new_venue = Venue(
+            venue_name=venue_name,
+            venue_email=venue_email,
+            venue_city=venue_city,
+            venue_state=venue_state,
+            venue_zip_code=venue_zip_code,
+            capacity=capacity,
+        )
+
+        session.add(new_venue)
+        session.commit()
+
+        print(f"Venue: {venue_name} created!")
+    else:
+        print(f"Venue: {venue_name} already exists!")
 
 
-def delete_venue():
-    ## filter by venue name
-    ## if venue available, remove venue
-    pass
+# filter by venue name
+# if venue available, remove venue
+def delete_venue(venue_name):
+    venue_to_delete = session.query(Venue).filter_by(venue_name=venue_name).first()
+
+    if venue_to_delete:
+        session.delete(venue_to_delete)
+        session.commit()
+
+        print(f"Venue: {venue_name} has been deleted!")
+    else:
+        print(f"Venue: {venue_name} does not exist.")
 
 
+# query by status and booking date
+# if booking status false, allow for booking
+# otherwise, return message booking not created
 def create_booking():
-    ##query by status and booking date
-    ## if booking status false, allow for booking
-    ## otherwise, return message booking not created
     pass
 
 
+# query by booking_date
+# remove booking, return message that confirmed
 def cancel_booking():
-    ##query by booking_date
-    ## remove booking, return message that confirmed
     pass
 
 
+# if true:
+# return message that venue is booked
+# otherwise, return message that venue is available
 def confirmed_booking():
-    ##if true:
-    ## return message that venue is booked
-    ## otherwise, return message that venue is available
     pass
